95e657f1d0581c977263fbaf737a8291c6543a30
DuncanT2000 <duncanthomson71@gmail.com>
2021-07-13 22:31:22 +0100

    Added BSC Links to right sidebar

--- src/components/RightSidebar/RightSidebar.js
+++ src/components/RightSidebar/RightSidebar.js
@@ -24,6 +24,32 @@ const RightSidebar = (props) => {
             TokenInfoItemValue={Number(Math.floor(props.TokenDetails.TokenSupply / `1${"0".repeat(props.TokenDetails.TokenDecimals)}`)).toLocaleString()}
              />
 
+            <TokenInfoItem
+            TokenInfoItemHeader="BSC Scan Links"
+            TokenInfoItemValue={
+            <div>
+                <a 
+                href={`https://bscscan.com/token/${props.TokenDetails.tokenAddress}`}
+                target="_blank"
+                ><p style={{
+                    color:'white'
+                }}>View Transactions</p></a>
+            <a 
+                href={`https://bscscan.com/address/${props.TokenDetails.tokenAddress}#code`}
+                target="_blank"
+                ><p style={{
+                    color:'white'
+                }}>View Contract</p></a>
+                <a 
+                href={`https://bscscan.com/token/${props.TokenDetails.tokenAddress}#balances`}
+                target="_blank"
+                >
+                    <p style={{
+                    color:'white'
+                }}>View Holders</p></a>
+                </div>}
+             />
+
 
 
         </div>
